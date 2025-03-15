4f751d5c60651839d2fce3a0c8563e7dd12d9308
CHEN1594 <360253231@qq.com>
2025-03-15 23:54:29 +0800

    add 'role' in vis_obj

--- chart_parser/extract_svg.py
+++ chart_parser/extract_svg.py
@@ -3,6 +3,9 @@ import subprocess
 import json
 import os
 import bs4
+from enhanced_axes_extraction.classify_element import classify_simvec
+from enhanced_axes_extraction.get_metadata import convert_svg_to_png
+from utils import pprint
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,6 +15,7 @@ if not os.path.isdir(PARSE_TMP_DIR):
 
 SVG_INPUT_PATH = os.path.join(BASE_DIR, f"{PARSE_TMP_DIR}/js_temp_svg.svg")
 JSON_OUTPUT_PATH = os.path.join(BASE_DIR, f"{PARSE_TMP_DIR}/svg_parse.json")
+PNG_OUTPUT_PATH = os.path.join(BASE_DIR, f"{PARSE_TMP_DIR}/js_temp_svg.png")
 
 def parse_unknown_svg_visual_elements(svg_string):
     """
@@ -21,6 +25,8 @@ def parse_unknown_svg_visual_elements(svg_string):
 
     with open(SVG_INPUT_PATH, "w", encoding="utf-8") as f:
         f.write(svg_string)
+    
+    convert_svg_to_png(SVG_INPUT_PATH,PNG_OUTPUT_PATH)
 
     script_path = os.path.join(BASE_DIR, "parse_svg_x.js")
     if not os.path.exists(script_path):
@@ -37,6 +43,29 @@ def parse_unknown_svg_visual_elements(svg_string):
     with open(JSON_OUTPUT_PATH, "r", encoding="utf-8") as f:
         data = json.load(f)
 
+    #调用classify_element.py中的函数
+    # 提取 SimVec 数据并调用 classify_simvec
+    simvec_data = data.get("sim_vector", "")
+    if not simvec_data:
+        raise ValueError("未找到有效的 SimVec 数据")
+
+    classified_data = classify_simvec(simvec_data, PNG_OUTPUT_PATH)
+
+    # 创建vid到role的映射字典
+    vid_to_role = {}
+    for item in classified_data:
+        for category, vids in item.items():
+            if category == "legend":
+                for subcategory, subvids in vids.items():
+                    vid_to_role.update({vid: subcategory for vid in subvids})
+            else:
+                vid_to_role.update({vid: category for vid in vids})
+
+    # 更新rects中的每个元素，添加role字段
+    for rect in data.get("rects", []):
+        vid = rect.get("vid")
+        rect["role"] = vid_to_role.get(vid, "other")
+
     rects_attr = data.get("rects", [])
     width = data.get("width", 1000)
     height = data.get("height", 1000)
@@ -44,7 +73,7 @@ def parse_unknown_svg_visual_elements(svg_string):
     soup = bs4.BeautifulSoup(svg_string, "html5lib")
     svg = soup.select_one("svg")
 
-    
+    pprint(rects_attr)
     
 
     return rects_attr, width, height, svg
